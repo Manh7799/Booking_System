5085621ba38407f09202ef3359d8e2788cd7f0ce
Admin <manhnt7799@gmail.com>
2025-07-22 14:46:28 +0700

    Initial

---
+++ src/main/java/com/example/booking_system/controller/NguoiDungController.java
@@ -0,0 +1,13 @@
+package com.example.booking_system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NguoiDungController {
+
+    @GetMapping("/nguoidung")
+    public String hienThidanhSachNguoiDung() {
+        return "admin/NguoiDungCallApi";
+    }
+}
